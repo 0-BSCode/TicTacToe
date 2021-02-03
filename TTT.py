3265f6996c6cc7b03a1bffe98fff92624cb9d398
Bryan <bryanipsanchez@gmail.com>
2021-02-03 20:19:07 +0800

    Revised winning mechanics

--- TTT.py
+++ TTT.py
@@ -3,14 +3,20 @@ import random, time
 board = [[' ',' ',' '],
          [' ',' ',' '],
          [' ',' ',' ']]
-end = False
+         
+end_results = {"P1W": False, "P2W": False, "CPU": False, "Draw": False}
 draw = False
 
+class gameFinished(Exception):
+    pass
 
 def select_mode(vs, symbol, order):
     # Initialize settings for game
     setup = dict() 
-    setup['versus'] = 0
+    if vs:
+        setup['versus'] = 1
+    else:
+        setup['versus'] = 0
 
     if symbol == "O":
         setup['symbol'] = "O"
@@ -39,8 +45,8 @@ def print_board():
 
 def check_board(sym):
     # Checks if a win or a draw occurs
-    global end, draw
     # Check the rows
+    global draw
     for row in range(3):
         row_count = 0
         for column in range(3):
@@ -48,7 +54,6 @@ def check_board(sym):
                 row_count += 1
         # print("Row count:", row_count)
         if row_count == 3:
-            end = True
             return True
 
     # Check columns
@@ -59,7 +64,6 @@ def check_board(sym):
                 column_count += 1
         # print("Column count:", column_count)
         if column_count == 3:
-            end = True
             return True
     
     # Check diagonals
@@ -69,10 +73,9 @@ def check_board(sym):
             diagonal_count[0] += 1
         if board[2-i][i] == sym:
             diagonal_count[1] += 1
-    print("Diagonal count:", diagonal_count)
+    # print("Diagonal count:", diagonal_count)
 
     if diagonal_count[0] == 3 or diagonal_count[1] == 3:
-        end = True
         return True
 
     # Check if board is full
@@ -89,21 +92,27 @@ def check_board(sym):
     return False        
     
 def player_move(symbol):
-    global end
+    global end_results
     while True:
         try:
             position = int(input("Player 1 move: "))
             if is_valid_pos(position, symbol):
                 if check_board(symbol):
-                    pass
-            break
+                    if draw:
+                        end_results['Draw'] = True
+                    else:
+                        end_results['P1W'] = True
+                    raise Exception
+                break
         except:
-            pass
+            # If result has been decided, print board one last time then end the game
+            print_board()
+            raise gameFinished
         print("Invalid move!")
     print_board()
 
 def opponent_move(symbol, versus):
-    global end
+    global end_results
     while True:
         if versus:
             print("Thinking....")
@@ -117,16 +126,29 @@ def opponent_move(symbol, versus):
                 if versus:
                     print("Opponent move:", position)
                 if check_board(symbol):
-                    pass
+                    if draw:
+                        end_results['Draw'] = True
+                    else:
+                        if versus:
+                            end_results['CPU'] = True
+                        else:
+                            end_results['P2W'] = True
+                    raise Exception
                 break
         except:
-            pass
+            # If a result has been decided, print board one last time then end the game
+            print_board()
+            raise gameFinished
         if not versus:
             print("Invalid move!")
     print_board()
     
 def is_valid_pos(pos, sym):
     # Checks if move is valid
+    
+    # Get rid of 0 case early
+    if pos == 0:
+        return False
 
     # Subtract one from position for zero-indexing
     pos -= 1
@@ -151,52 +173,23 @@ def main():
     print("\n3. The game ends when you or your opponent place an X or O along the same column, row, or diagonal")
     versus = input("Who is your opponent? [0 = player, 1 = computer] ")
     symbol = input("What symbol do you want to use? [Choices: O, X] ")
-    order = input("Do you want to go first? [y/n]" )
+    order = input("Do you want to go first? [y/n] ")
     FINAL_SETUP = select_mode(versus, symbol, order)
-    # print(FINAL_SETUP)
-    while not end:
-        if FINAL_SETUP["order"]:
-            player_move(FINAL_SETUP["symbol"])
-            if end:
-                if FINAL_SETUP["versus"]:
-                    print("You win!")
-                else:
-                    print("Player 1 wins!")
-                break
-            elif draw:
-                print("Draw!")
-                break
-            opponent_move(FINAL_SETUP["o_symbol"], FINAL_SETUP["versus"])
-            if end:
-                if FINAL_SETUP["versus"]:
-                    print("You lose.")
-                else:
-                    print("Player 2 wins!")
-                break
-            elif draw:
-                print("Draw!")
-                break
-        else:
-            opponent_move(FINAL_SETUP["o_symbol"], FINAL_SETUP["versus"])
-            if end:
-                if FINAL_SETUP["versus"]:
-                    print("You lose.")
-                else:
-                    print("Player 2 wins!")
-                break
-            elif draw:
-                print("Draw!")
-                break
-            player_move(FINAL_SETUP["symbol"])
-            if end:
-                if FINAL_SETUP["versus"]:
-                    print("You win!")
-                else:
-                    print("Player 1 wins!")
-                break
-            elif draw:
-                print("Draw!")
-                break
+    try:
+        while True:
+            if FINAL_SETUP["order"]:
+                player_move(FINAL_SETUP["symbol"])
+                opponent_move(FINAL_SETUP["o_symbol"], FINAL_SETUP["versus"])
+            else:
+                opponent_move(FINAL_SETUP["o_symbol"], FINAL_SETUP["versus"])
+                player_move(FINAL_SETUP["symbol"])
+    except gameFinished:
+        if end_results["P1W"]:
+            print("Player 1 wins!")
+        elif end_results["P2W"]:
+            print("Player 2 wins!")
+        elif end_results["CPU"]:
+            print("You lose.")
     print("Thank you for playing!")
 
 if __name__=="__main__":
